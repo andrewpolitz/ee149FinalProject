87931f5084ab75c459655d9295de4398811aa2a2
andrewpolitz <andrewpolitz@berkeley.edu>
2024-12-16 21:23:40 -0800

    Add polling for sensors

--- hierarchical_state_machine/hierarchical_state_machine.ino
+++ hierarchical_state_machine/hierarchical_state_machine.ino
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <Ticker.h>
 #include <Adafruit_MCP9808.h>
 #include <Adafruit_AMG88xx.h>
 #include <Adafruit_NeoPixel.h>
+
+#define SCL 22 // SCL PIN on our ESP32 
+#define SDA 23 // SDA PIN on our ESP32
 
 #define CLK_PIN 27    // Rotary encoder clock pin
 #define DT_PIN 33     // Rotary encoder data pin
@@ -21,26 +25,54 @@
 #endif
 #define LED_COUNT 12
 
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi
+const char *ssid = "NETGEAR82"; // Enter your Wi-Fi name
+const char *password = "icypiano627";  // Enter Wi-Fi password
+
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "IoTFan/esp32";
+const char *mqtt_username = "IoTFan";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 // Variables
 int power = 0; // Range 0-100
 int curr_temp; // Current temperature
 int pref_temp = 24; // Preferred temperature
-int camera_max = 0;
+int camera_max; // max camera pixel temp
+
 bool present = false;
 bool sw_press = false;
+bool prev_press = false;
 bool e_tick = false;
 bool e_dir = false;
 bool camera_flag = false;
+bool camera_active = false;
+
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50; // 50 ms debounce delay
 
 // Create Temperature Sensor object
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
 // AMG88xx IR Sensor
-Adafruit_AMG88xx amg;
+Adafruit_AMG88xx camera;
 float pixels[AMG_COLS * AMG_ROWS]; // Sensor 1D data array
 
 // Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, NEO_PIN, NEO_GRB + NEO_KHZ800);
+
+Ticket presentTimer;
+Ticker tempTimer;
+Ticker cameraTimer;
+Ticker mqttTimer;
 
 // States
 enum MainState { OFF, MANUAL, AUTOMATIC };
@@ -57,12 +89,11 @@ void updateDutyCycle();
 
 void setup() {
 
-
   Serial.begin(115200);
 
   // Rotaty Encoder
-  pinMode(CLK_PIN, INPUT);
-  pinMode(DT_PIN, INPUT);
+  pinMode(CLK_PIN, INPUT_PULLUP);
+  pinMode(DT_PIN, INPUT_PULLUP);
   pinMode(SW_PIN, INPUT_PULLUP);
 
   // Fan PWN
@@ -71,65 +102,88 @@ void setup() {
   // PIR Sensor
   pinMode(PIR_PIN, INPUT);
 
+  // Set up I2C communication pins
+  Wire.begin(SDA, SCL);
+
   // Initialize Temperature Sensor
   if (!tempsensor.begin(0x18)) { // Default I2C address is 0x18
     Serial.println("Couldn't find MCP9808 sensor! Check wiring.");
     while (1); // Halt execution
   }
+
   Serial.println("MCP9808 sensor initialized.");
+
+ // initialize thermal camera
+   if (!camera.begin()) {
+    Serial.println("Could not find a valid AMG88xx sensor. Check wiring!");
+    while (1) delay(1);
+  }
   
+  Serial.println("AMG8833 Thermal Camera initialized!");
+
+  
+  // start the temp sensor
   tempsensor.setResolution(3); // Set resolution: 0 - low (0.5°C), 1 - medium (0.25°C), 2 - high (0.125°C), 3 - max (0.0625°C)
   tempsensor.wake(); // start temp sensor
   
   delay(250); // let temp sensor stabilize
 
   updateCurrTemp(); // ensures an initial temperature value
+  updateCameraMax(); // grabs an initial picture of the environment
 
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
   #endif
+
+  // configure wifi
+  configureMQTT();
+
+  // set up polling timers
+  presentTimer.attach(5.0, checkPresent); // checks if someone is present every 5 seconds
+  tempTimer.attach(5.0, updateCurrTemp); // updates the temperature every 5 seconds
+  
 }
 
 void loop() {
-  // Check for button clicks to switch modes
-  if (sw_press) {
-    if (mainState == OFF) mainState = MANUAL;
-    else if (mainState == MANUAL) mainState = AUTOMATIC;
-    else mainState = OFF;
-    sw_press = false; // Reset button press flag
-  }
+
+  client.loop();
+
+  updateMainState();
 
   // State handling
   switch (mainState) {
     case OFF:
+      Serial.println("In Off Mode");
       power = 0;
       camera_flag = false;
       updateDutyCycle();
       break;
 
     case MANUAL:
+      Serial.println("In Manual Mode");
       handleManualState();
       break;
 
     case AUTOMATIC:
+      Serial.println("In Automatic Mode");
       handleAutomaticState();
+      Serial.println(curr_temp);
+      Serial.println(camera_max);
       break;
   }
 }
 
 
 void handleManualState() {
-  checkPresent();
-  updateCurrTemp();
 
   if (!present) {
     subState = SUB_OFF;
     power = 0;
-  } elif (e_tick) {
+  } else if (e_tick) {
 
     if (e_dir && power < 100) {
       power += 10; // Increase power
-    } elif (!e_dir && power > 0) {
+    } else if (!e_dir && power > 0) {
       power -= 10; // Decrease power
     }
 
@@ -141,9 +195,6 @@ void handleManualState() {
 }
 
 void handleAutomaticState() {
-
-  checkPresent();
-  updateCurrTemp();
 
   if (!present) {
     subState = SUB_OFF;
@@ -163,14 +214,30 @@ void handleAutomaticState() {
   updateDutyCycle();
 }
 
+void updateMainState() {
+
+  bool buttonState = digitalRead(SW_PIN);
+
+  if (buttonState != prev_press) {
+    if (millis() - lastDebounceTime > debounceDelay) {
+      lastDebounceTime = millis();
+      
+      if (buttonState == LOW) {
+        mainState = static_cast<MainState>((mainState + 1) % 3);
+      } 
+    }
+  }
+  prev_press = buttonState;
+}
+
 void updateDutyCycle() {
   // Simulate duty cycle with LED brightness
-  analogWrite(LED_BUILTIN, map(power, 0, 100, 0, 255));
+  analogWrite(FAN_PWM_PIN, map(power, 0, 100, 0, 255));
 }
 
 void updateCameraMax() {
 
-  amg.readPixels(pixels);
+  camera.readPixels(pixels);
 
   int maxTemp = pixels[0];
   for(int i = 1; i < AMG_COLS * AMG_ROWS; i++) {
@@ -185,6 +252,8 @@ void updateCameraMax() {
 void updateCurrTemp() {
     // updates the temp sensor
     curr_temp = tempsensor.readTempC();
+    String output = "curr_temp" + String(curr_temp);
+    client.publish(topic, "temp");
 }
 
 void checkPresent() {
@@ -194,8 +263,13 @@ void checkPresent() {
   if(pir_present){
     camera_flag = true; // flag camera
   } 
-  if(camera_flag) {
-     updateCameraMax();
+
+  if(camera_flag && !camera_active) {
+     cameraTimer.attach(5.0, updateCameraMax);
+     camera_active = true;
+  } else {
+    cameraTimer.detach();
+    camera_active = false;
   }
 
   bool camera_present = (camera_max > curr_temp + 2);
@@ -207,4 +281,43 @@ void checkPresent() {
 
   // Reset camera_flag if no presence is detected
   camera_flag = present ? camera_flag : false;
+}
+
+void configureMQTT() {
+    //WIFI + MQTT Set Up
+  WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the Wi-Fi network");
+    //connecting to a mqtt broker
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    while (!client.connected()) {
+        String client_id = "esp32-client-";
+        client_id += String(WiFi.macAddress());
+        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+            Serial.println("Public EMQX MQTT broker connected");
+        } else {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+    // Publish and subscribe
+    client.publish(topic, "Hi, I'm your ESP32 smart fan ^^");
+    client.subscribe(topic);
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
 }
