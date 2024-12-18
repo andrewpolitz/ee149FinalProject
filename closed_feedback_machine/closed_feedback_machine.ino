b937d7187ee01316828b47fbf4a92ab9670aea9a
andrewpolitz <andrewpolitz@berkeley.edu>
2024-12-18 13:59:37 -0800

    Fixed manual state by adding encoder logic

--- closed_feedback_machine/closed_feedback_machine.ino
+++ closed_feedback_machine/closed_feedback_machine.ino
@@ -5,23 +5,23 @@
 #include <Adafruit_AMG88xx.h>
 #include <Adafruit_NeoPixel.h>
 
-#define SCL 22 // SCL PIN on our ESP32 
-#define SDA 23 // SDA PIN on our ESP32
+#define SCL 22  // SCL PIN on our ESP32
+#define SDA 23  // SDA PIN on our ESP32
 
-#define CLK_PIN 27    // Rotary encoder clock pin
-#define DT_PIN 33     // Rotary encoder data pin
-#define SW_PIN 15     // Rotary encoder switch pin
+#define CLK_PIN 27  // Rotary encoder clock pin
+#define DT_PIN 33   // Rotary encoder data pin
+#define SW_PIN 15   // Rotary encoder switch pin
 
-#define FAN_PWM_PIN 21 // PWM output pin for fan
+#define FAN_PWM_PIN 21  // PWM output pin for fan
 
-#define AMG_COLS 8    // IR Camera col
-#define AMG_ROWS 8    // IR Camera row
+#define AMG_COLS 8  // IR Camera col
+#define AMG_ROWS 8  // IR Camera row
 
-#define PIR_PIN 32    // PIR Sensor Pin
+#define PIR_PIN 32  // PIR Sensor Pin
 
-#define NEO_PIN 14    // NeoPixel Pin
+#define NEO_PIN 14  // NeoPixel Pin
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
 #endif
 #define LED_COUNT 12
 
@@ -29,7 +29,7 @@
 #include <PubSubClient.h>
 
 // WiFi
-const char *ssid = "NETGEAR_CBE"; // Enter your Wi-Fi name
+const char *ssid = "NETGEAR_CBE";      // Enter your Wi-Fi name
 const char *password = "happybug682";  // Enter Wi-Fi password
 
 // MQTT Broker
@@ -43,31 +43,29 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Variables
-int power = 0; // Range 0-100
+int power = 0;  // Range 0-100
 int current_power = 0;
-int curr_temp; // Current temperature
-int pref_temp = 10; // Preferred temperature
-int camera_max; // max camera pixel temp
+int curr_temp;       // Current temperature
+int pref_temp = 24;  // Preferred temperature
+int camera_max;      // max camera pixel temp
 
 bool present = false;
 bool sw_press = false;
 bool prev_press = false;
-int clk_count = 0;
-int curr_clk;
-int prev_clk;
+bool e_tick = false;
 bool e_dir = false;
 bool camera_flag = false;
 bool camera_active = false;
 
 unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 50; // 50 ms debounce delay
+const unsigned long debounceDelay = 50;  // 50 ms debounce delay
 
 // Create Temperature Sensor object
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
 // AMG88xx IR Sensor
 Adafruit_AMG88xx camera;
-float pixels[AMG_COLS * AMG_ROWS]; // Sensor 1D data array
+float pixels[AMG_COLS * AMG_ROWS];  // Sensor 1D data array
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, NEO_PIN, NEO_GRB + NEO_KHZ800);
@@ -79,8 +77,11 @@ Ticker cameraTimer;
 Ticker mqttTimer;
 
 // States
-enum MainState { OFF, MANUAL, AUTOMATIC };
-enum SubState { SUB_OFF, SUB_ON };
+enum MainState { OFF,
+                 MANUAL,
+                 AUTOMATIC };
+enum SubState { SUB_OFF,
+                SUB_ON };
 
 MainState mainState = OFF;
 SubState subState = SUB_OFF;
@@ -110,36 +111,35 @@ void setup() {
   Wire.begin(SDA, SCL);
 
   // Initialize Temperature Sensor
-  while (!tempsensor.begin(0x18)) { // Default I2C address is 0x18
+  if (!tempsensor.begin(0x18)) {  // Default I2C address is 0x18
     Serial.println("Couldn't find MCP9808 sensor! Check wiring.");
-    Serial.println("Try connecting the board to power.");
-    delay(1000);
-     // Halt execution
+    while (1)
+      ;  // Halt execution
   }
 
   Serial.println("MCP9808 sensor initialized.");
 
- // initialize thermal camera
-   while (!camera.begin()) {
+  // initialize thermal camera
+  if (!camera.begin()) {
     Serial.println("Could not find a valid AMG88xx sensor. Check wiring!");
-    delay(1000);
+    while (1) delay(1);
   }
-  
+
   Serial.println("AMG8833 Thermal Camera initialized!");
 
-  
+
   // start the temp sensor
-  tempsensor.setResolution(3); // Set resolution: 0 - low (0.5°C), 1 - medium (0.25°C), 2 - high (0.125°C), 3 - max (0.0625°C)
-  tempsensor.wake(); // start temp sensor
-  
-  delay(250); // let temp sensor stabilize
+  tempsensor.setResolution(3);  // Set resolution: 0 - low (0.5°C), 1 - medium (0.25°C), 2 - high (0.125°C), 3 - max (0.0625°C)
+  tempsensor.wake();            // start temp sensor
 
-  updateCurrTemp(); // ensures an initial temperature value
-  updateCameraMax(); // grabs an initial picture of the environment
+  delay(250);  // let temp sensor stabilize
 
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  updateCurrTemp();   // ensures an initial temperature value
+  updateCameraMax();  // grabs an initial picture of the environment
+
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
-  #endif
+#endif
 
   // configure wifi
   configureMQTT();
@@ -148,14 +148,10 @@ void setup() {
   strip.begin();
   strip.show();
 
-  // get first encoder reading
-  prev_clk = digitalRead(CLK_PIN);
-
   // set up polling timers
-  presentTimer.attach(5.0, checkPresent); // checks if someone is present every 5 seconds
-  tempTimer.attach(5.0, updateCurrTemp); // updates the temperature every 5 seconds
-  mqttTimer.attach(10.0, publishTempState); // sets timer to publish mqtt data
-  
+  presentTimer.attach(5.0, checkPresent);    // checks if someone is present every 5 seconds
+  tempTimer.attach(5.0, updateCurrTemp);     // updates the temperature every 5 seconds
+  mqttTimer.attach(10.0, publishTempState);  // sets timer to publish mqtt data
 }
 
 void loop() {
@@ -189,14 +185,17 @@ void loop() {
 
 void handleManualState() {
 
-  readEncoder();
-
   if (!present) {
     subState = SUB_OFF;
     power = 0;
-  } else {
-    power = clk_count;
-  } 
+  } else if (e_tick) {
+
+    if (e_dir && power < 100) {
+      power += 10;  // Increase power
+    } else if (!e_dir && power > 0) {
+      power -= 10;  // Decrease power
+    }
+  }
 
   // Update substate
   subState = (power > 0) ? SUB_ON : SUB_OFF;
@@ -209,16 +208,15 @@ void handleAutomaticState() {
     subState = SUB_OFF;
     power = 0;
   } else {
-    if (curr_temp < pref_temp) {
-      int diff = pref_temp - curr_temp;
+    if (curr_temp < camera_max) {
+      int diff = camera_max - curr_temp;
       if (diff >= 20) {
         power = 100;  // Max power
-      }      
-      else { 
+      } else {
         power = 5 * diff;
-      }   // Proportional power
+      }  // Proportional power
     } else {
-      power = 0; // Turn off
+      power = 0;  // Turn off
     }
   }
 
@@ -227,28 +225,6 @@ void handleAutomaticState() {
   updateDutyCycle();
 }
 
-void readEncoder() {
-
-    curr_clk = digitalRead(CLK_PIN);
-    
-	  if (curr_clk != prev_clk && curr_clk == 1){
-
-      if (digitalRead(DT_PIN) != curr_clk) {
-        // Counter clockwise rotation
-        // set range to min 0
-        if (clk_count > 0){
-          clk_count -= 10;
-        }
-      } else {
-        // Clockwise rotation
-        // set range to max 100
-        if (clk_count < 100) {
-          clk_count += 10;
-        }
-      }
-    }
- }
-
 void updateMainState() {
 
   bool buttonState = digitalRead(SW_PIN);
@@ -256,12 +232,12 @@ void updateMainState() {
   if (buttonState != prev_press) {
     if (millis() - lastDebounceTime > debounceDelay) {
       lastDebounceTime = millis();
-      
+
       if (buttonState == LOW) {
         //on press advances state and updates the led colors
         mainState = static_cast<MainState>((mainState + 1) % 3);
         transitionLED(mainState);
-      } 
+      }
     }
   }
   prev_press = buttonState;
@@ -270,27 +246,26 @@ void updateMainState() {
 void transitionLED(MainState nextState) {
   // switch statement to blink the led's during a transition
   switch (nextState) {
-      case OFF:
-        Serial.println("OFF");
-        blink(strip.Color(255, 0, 0), 2, 200);
-        break;
-      case MANUAL:
-        Serial.println("MANUAL");
-        blink(strip.Color(10, 55, 255), 2, 200);
-        break;
-      case AUTOMATIC:
-        Serial.println("AUTOMATIC");
-        blink(strip.Color(85, 255, 9), 2, 200);
-        break;
+    case OFF:
+      Serial.println("OFF");
+      blink(strip.Color(255, 0, 0), 2, 200);
+      break;
+    case MANUAL:
+      Serial.println("MANUAL");
+      blink(strip.Color(10, 55, 255), 2, 200);
+      break;
+    case AUTOMATIC:
+      Serial.println("AUTOMATIC");
+      blink(strip.Color(85, 255, 9), 2, 200);
+      break;
   }
-
 }
 
 void blink(uint32_t color, int numBlinks, int interval) {
-  for (int i = 0; i < numBlinks; i++){
+  for (int i = 0; i < numBlinks; i++) {
     fillStrip(color);
     delay(interval);
-    fillStrip(strip.Color(0,0,0));
+    fillStrip(strip.Color(0, 0, 0));
     delay(interval);
   }
 }
@@ -302,39 +277,37 @@ void fillStrip(uint32_t color) {
   strip.show();
 }
 
-void lightLEDs(uint32_t color){
+void lightLEDs(uint32_t color) {
 
   // maps the power input to the number of leds
   int ledsToLight = map(power, 0, 100, 0, 12);
 
   // turns of lights starting at the top
-  for(int i = LED_COUNT - 1; i > ledsToLight; i--){
+  for (int i = LED_COUNT - 1; i > ledsToLight; i--) {
     strip.setPixelColor(i, 0);
     strip.show();
     delay(50);
   }
   // turns on lights starting at bottom
-  for(int i = 0; i <= ledsToLight; i++){
+  for (int i = 0; i <= ledsToLight; i++) {
     strip.setPixelColor(i, color);
     delay(50);
   }
-
 }
 
 void updateDutyCycle() {
   // Simulate duty cycle with LED brightness
-  if(power == 0) {
+  if (power == 0) {
     analogWrite(FAN_PWM_PIN, 0);
     current_power = 0;
   } else {
     if (current_power == 0) {
-       analogWrite(FAN_PWM_PIN, 255);
-       delay(500);
+      analogWrite(FAN_PWM_PIN, 255);
+      delay(500);
     }
-    analogWrite(FAN_PWM_PIN, map(power, 0, 100, 120, 255));
+    analogWrite(FAN_PWM_PIN, map(power, 0, 100, 55, 255));
     current_power = power;
   }
-  
 }
 
 void updateCameraMax() {
@@ -342,7 +315,7 @@ void updateCameraMax() {
   camera.readPixels(pixels);
 
   int maxTemp = pixels[0];
-  for(int i = 1; i < AMG_COLS * AMG_ROWS; i++) {
+  for (int i = 1; i < AMG_COLS * AMG_ROWS; i++) {
     if (pixels[i] > maxTemp) {
       maxTemp = pixels[i];
     }
@@ -352,8 +325,10 @@ void updateCameraMax() {
 }
 
 void updateCurrTemp() {
-    // updates the temp sensor
-    curr_temp = tempsensor.readTempC();
+  // updates the temp sensor
+  curr_temp = tempsensor.readTempC();
+  String output = "curr_temp" + String(curr_temp);
+  client.publish(topic, output.c_str());
 }
 
 void checkPresent() {
@@ -361,14 +336,14 @@ void checkPresent() {
   Serial.println("Checked Presence");
   bool pir_present = digitalRead(PIR_PIN) == HIGH;
 
-  if(pir_present){
-    camera_flag = true; // flag camera
+  if (pir_present) {
+    camera_flag = true;  // flag camera
     Serial.println("Motion Detected");
     cameraTimer.attach(2.0, updateCameraMax);
-  } 
+  }
 
-  if(!camera_flag) {
-     cameraTimer.detach();
+  if (!camera_flag) {
+    cameraTimer.detach();
   }
 
   bool camera_present = (camera_max > curr_temp + 2);
@@ -378,62 +353,49 @@ void checkPresent() {
 
   // Reset camera_flag if no presence is detected
   camera_flag = present ? camera_flag : false;
-  
 }
 
 void publishTempState() {
 
-  String output = " curr_temp: " + String(curr_temp) + "\n camera_max: " +  String(camera_max) + " \n Present? " + String(present);
+  String output = "curr_temp: " + String(curr_temp) + "\n camera_max: " + String(camera_max) + " \n Present? " + String(present);
   client.publish(topic, output.c_str());
-
 }
 
 void configureMQTT() {
-    //WIFI + MQTT Set Up
+  //WIFI + MQTT Set Up
   WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.println("Connecting to WiFi..");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the Wi-Fi network");
+  //connecting to a mqtt broker
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    String client_id = "esp32-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("Public EMQX MQTT broker connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
     }
-    Serial.println("Connected to the Wi-Fi network");
-    //connecting to a mqtt broker
-    client.setServer(mqtt_broker, mqtt_port);
-    client.setCallback(callback);
-    while (!client.connected()) {
-        String client_id = "esp32-client-";
-        client_id += String(WiFi.macAddress());
-        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
-        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-            Serial.println("Public EMQX MQTT broker connected");
-        } else {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
-            delay(2000);
-        }
-    }
-    // Publish and subscribe
-    client.publish(topic, "Hi, I'm your ESP32 smart fan ^^");
-    client.subscribe(topic);
+  }
+  // Publish and subscribe
+  client.publish(topic, "Hi, I'm your ESP32 smart fan ^^");
+  client.subscribe(topic);
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
-
-    
-    Serial.print("Message arrived in topic: ");
-    Serial.println(topic);
-    Serial.print("Message:");
-    String message;
-
-    for (int i = 0; i < length; i++) {
-        message += (char) payload[i];
-    }
-
-    int recieved_temp = message.toInt();
-    if(recieved_temp > 0) {
-      //attempt to read incoming message to update preference temperature
-      pref_temp = recieved_temp;
-    }
-    Serial.println(message);
-    Serial.println();
-    Serial.println("-----------------------");
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
 }
